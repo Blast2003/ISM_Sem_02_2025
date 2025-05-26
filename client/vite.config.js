b039b31edacfb0afdff4de2f0d5b73f85dd7d53e
Blast1 <phih2k3@gmail.com>
2025-05-26 20:40:18 +0700

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,10 +5,6 @@ import { defineConfig } from "vite"
 export default defineConfig({
   plugins: [react()],
   server: {
-    headers: {
-      'Cross-Origin-Opener-Policy': 'unsafe-none',
-      'Cross-Origin-Embedder-Policy': 'unsafe-none'
-    }
   },
   resolve: {
     alias: {
